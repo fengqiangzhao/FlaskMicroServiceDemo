6202ddb61f278ffe38a004cf906580a13ed37e33
zhaofengqiang <zhaofengqiang@zhixuehd.com>
2019-12-10 18:59:35 +0800

    add react web page

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 
 def create_app(config):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config)
     register_extensions(app)
     register_blueprint(app)
